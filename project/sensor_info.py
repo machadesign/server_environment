a3395b261b48cee9421357cc2ecbfbc1d4561bbd
machadesign <32038084+codejelly@users.noreply.github.com>
2020-10-17 23:09:56 -0500

    gitignore .idea and test file and project dir added

---
+++ project/sensor_info.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# This script reads,checks the temp and returns it in the intended format
+# Input required temp_id to acquire temperature reading for the probe
+
+import re
+
+
+temp_directory = '/Users/matthewchadwell/mock_temp/temp_id/'
+# Test - Mock data locally stored in a .txt file
+# Temp_id = 'id0101'
+
+
+def temp_function(id):
+    # Return a positive or negative value for temp if CRC is good
+    temp_file = temp_directory + id
+    with open(temp_file) as temp_readline:
+        # reads first line of the file , checks CRC(reading good or bad)
+        temp_crc = temp_readline.readline()
+        temp_crc_check = re.search(r'YES$', temp_crc)
+        if temp_crc_check:
+            temp_line = temp_readline.readline()
+            # read the next line from temp output
+            negative_check = re.search(r'(t=-\d*)', temp_line)
+# TODO (currently works w/o positive check first,would like to change) positive_check = re.search(r'(t=\d*)', check)
+            positive_number = re.findall(r't=(\d*)', temp_line)
+            negative_number = re.findall(r't=(-\d*)', temp_line)
+            if negative_check is None:
+                return positive_number
+            elif negative_check:
+                return negative_number
+            else:
+                return 'error'
+        else:
+            return 999999
+
+
+def temp_formatted(current_temp):
+    if current_temp == 999999:
+        rounded_temp = 999999
+        return rounded_temp
+    else:
+        temp_int = int(current_temp[0])
+        temp_data = temp_int / 1000
+        rounded_temp = format(temp_data, '.2f')
+        return rounded_temp
+
+
+def main(id_sensor):
+    temp = temp_function(id_sensor)
+    formatted_temp = temp_formatted(temp)
+    print(formatted_temp)
+
+
+if __name__ == "__main__":
+    sesnor_id = 'id0101'
+    main(sesnor_id)
